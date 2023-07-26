9627565366d2dfa7d7979fd45644c9f04f07a17b
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-07-25 19:35:30 -0700

    Edit main.c

--- main.c
+++ main.c
@@ -10,22 +10,22 @@
 int main(int ac __attribute__((unused)),
 		 char **av __attribute__((unused)), char **env)
 {
-	char *linetptr = NULL;
+	char *line = NULL;
 	int inter_active = isatty(STDIN_FILENO);
-	size_t length = 0;
+	size_t len = 0;
 
 	if (inter_active)
 		/* only print the prompt if the shell is launched in interactive mode */
 		_puts("$", 0);
 
 	/* Loop until the user terminates with Ctrl + D */
-	while (getline(&line, &length, stdin) != -1)
+	while (getline(&line, &len, stdin) != -1)
 	{
-		if (linetptr[0] == '\0' || linetptr[0] == '\n')
+		if (line[0] == '\0' || line[0] == '\n')
 			continue;
-		execute(linetptr, env);
-/*		_puts("$", 0);*/
+		execute(line, env);
+		_puts("$", 0);
 	}
-	free(linetptr);
+	free(line);
 	return (0);
 }
