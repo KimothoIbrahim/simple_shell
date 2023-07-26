9e649044953a3d734ed7d7550d25254e73f2bc27
KimothoIbrahim <kimothoibrahim@yahoo.com>
2023-07-26 02:41:32 -0700

    Add simple_shell 2.0

---
+++ path.c
@@ -0,0 +1,29 @@
+#include "header.h"
+
+/**
+ * path_directories - makes paths and searches for a program in set folders
+ * @program: what the user typed as a command on prompt
+ * @found_program: string pointer set when program was found successfully
+ */
+
+void path_directories(char *program, char **found_program)
+{	int i;
+	char *path_value = getenv("PATH"), *cap;
+	char *token, *cat1, *cat2;
+	struct stat st;
+
+	cap = _strcpy(path_value);
+
+	for (i = 0; ; i++, cap = NULL)
+	{
+		token = strtok(cap, ":");
+		if (token == NULL)
+			break;
+		cat1 = _strconcat(token, "/");
+		cat2 = _strconcat(cat1, program);
+		if (stat(cat2, &st) == 0)
+			*found_program = cat2;
+		else if (stat(program, &st) == 0)
+			*found_program = program;
+	}
+}
---
+++ prompt.c
@@ -0,0 +1,39 @@
+#include "header.h"
+#define UNUSED(x) (void)(x)
+/**
+ * main - Entry prompt program: takes in commands and executes them
+ * @ac: Number of arguments provided by user
+ * @av: string array of all arguments obtained from user
+ * @env: Null terminated array enviroment variables
+ *
+ * Return: returns 0
+ */
+
+int main(int ac, char **av, char **env)
+{
+	char **stringArgumentsArray;
+	char *commandlineArgs = NULL, *found_program = NULL;
+	size_t n = 0;
+	ssize_t charactersRead;
+
+	UNUSED(ac);
+	UNUSED(av);
+
+
+	/*_puts("simple_Shell ($) ");*/
+	while ((charactersRead = getline(&commandlineArgs, &n, stdin)) != -1)
+	{
+		stringArgumentsArray = tokenizer(commandlineArgs);
+
+		if (_strcmp(stringArgumentsArray[0], "exit") == 0)
+		{
+			free(commandlineArgs);
+			exit(0);
+		}
+		path_directories(stringArgumentsArray[0], &found_program);
+		execute(found_program, stringArgumentsArray, env);
+		found_program = NULL;
+	}
+	free(commandlineArgs);
+	return (0);
+}
